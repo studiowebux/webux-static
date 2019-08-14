051e3a71540f1523a581862fe48596cd6e889fba
tommy gingras <tommy@studiowebux.com>
2019-08-13 20:44:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Webux Static
+
+This module uses express to serve static files
+
+## Installation
+
+```bash
+npm i --save webux-static
+```
+
+## Usage
+
+How it works,  
+You have to provide an array of path and local directory, the module will automatically serve those paths to external access.
+
+### The options
+
+```
+module.exports = {
+  resources: [
+    { path: "/api", resource: "./api" },
+    { path: "/assets", resource: "./assets" }
+  ]
+};
+```
+
+### Example
+
+**Note** You must load this module at the end to avoid issues with the API routes.
+
+```
+const express = require("express");
+const app = express();
+const { serveStatic } = require("webux-static");
+
+app.get("/api/v1/resource", (req, res) => {
+  res.status(200).json({ msg: "API CALL !" });
+});
+
+serveStatic(options, app, express)
+  .then(() => {
+    app.listen(1337, () => {
+      console.log("Server listening on port 1337 ... ");
+    });
+  })
+  .catch(e => {
+    console.error(e);
+  });
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+SEE LICENSE IN license.txt
---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * File: index.js
+ * Author: Tommy Gingras
+ * Date: 2019-10-13
+ * License: All rights reserved Studio Webux S.E.N.C 2015-Present
+ */
+
+"use strict";
+
+/**
+ * Load static resources
+ * @param {Object} options The configuration to start the server, mandatory
+ * @param {Function} app An express application, mandatory
+ * @param {Function} express The express object, mandatory
+ * @param {Function} log The log function, optional, by default console
+ * @returns {Promise}
+ */
+const serveStatic = (options, app, express, log = console) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (!options || typeof options !== "object") {
+        return reject(
+          new Error("The options parameter is required and must be an object")
+        );
+      }
+      if (!app || typeof app !== "function") {
+        return reject(
+          new Error("The app parameter is required and must be a function")
+        );
+      }
+      if (!express || typeof express !== "function") {
+        return reject(
+          new Error("The express parameter is required and must be a function")
+        );
+      }
+      if (log && typeof log !== "object") {
+        return reject(new Error("The log parameter must be an object"));
+      }
+
+      options.resources.forEach(resource => {
+        log.info("Link ", resource.path, " to ", resource.resource);
+        app.use(resource.path, express.static(resource.resource));
+      });
+
+      return resolve("Static resources loaded !");
+    } catch (e) {
+      throw e;
+    }
+  });
+};
+
+module.exports = { serveStatic };
